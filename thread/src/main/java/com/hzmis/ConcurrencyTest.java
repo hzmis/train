073af225e6e2f920f29d6a513c96f9d292f8654f
hz <wb-hz337696@alibaba-inc.com>
2018-04-01 22:25:06 +0800

    add

--- thread/src/main/java/com/hzmis/ConcurrencyTest.java
+++ thread/src/main/java/com/hzmis/ConcurrencyTest.java
@@ -4,5 +4,43 @@ package com.hzmis;
  * @author elephant-huang-zhao
  * @date 2018/3/31
  */
-public class ThreadCompare {
+public class ConcurrencyTest {
+    public static final long count = 1000000001;
+
+    public static void main(String[] args) throws InterruptedException {
+        concurency();
+        serial();
+    }
+
+    private static void serial() {
+        long begin = System.currentTimeMillis();
+        int a = 0, b = 0;
+        for (int i = 0; i < count; i++) {
+            a += 5;
+        }
+        for (int i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - begin;
+        System.out.println("serial :" + time + "ms , a: " + a + ", b: " + b);
+    }
+
+    private static void concurency() throws InterruptedException {
+        long begin = System.currentTimeMillis();
+        Thread thread = new Thread(() -> {
+            int a = 0;
+            for (int i = 0; i < count; i++) {
+                a += 5;
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (int i = 0; i < count; i++) {
+            b--;
+        }
+        thread.join();
+        long time = System.currentTimeMillis() - begin;
+        System.out.println("concurrency :" + time + "ms, b: " + b);
+
+    }
 }
--- thread/src/main/java/com/hzmis/NoVisibility.java
+++ thread/src/main/java/com/hzmis/NoVisibility.java
@@ -5,4 +5,22 @@ package com.hzmis;
  * @date 2018/4/1
  */
 public class NoVisibility {
+    private static boolean ready;
+    private static int number;
+
+    private static class ReaderThread extends Thread {
+        @Override
+        public void run() {
+            while (!ready) {
+                Thread.yield();
+                System.out.println(number);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReaderThread().start();
+        number = 33;
+        ready = true;
+    }
 }
